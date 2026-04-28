35a32fb94071d141875be99ec06a9bbea7b42d65
rain <2474441419@qq.com>
2026-04-28 23:15:06 +0800

    update

--- config/db.js
+++ config/db.js
@@ -2,6 +2,12 @@ const mongoose = require('mongoose');
 
 const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
 
-mongoose.connect(DB).then(() => {
-  console.log('DB connection successful!');
-});
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log('DB connection successful!');
+  })
+  .catch((err) => {
+    console.error('DB connection error:', err.message);
+    process.exit(1); // 连接失败时退出程序
+  });
